d26208dfbf9d503460ae346bf768dd3d67d8fdd5
ryan.b <ryan.b@argodevops.co.uk>
2023-05-18 07:45:51 +0000

    fix: removed teams notifications

--- release.config.js
+++ release.config.js
@@ -9,74 +9,9 @@ const branches = [
     }
 ];
 
-const commitAnalyzer = [
-    '@semantic-release/commit-analyzer',
-    {
-        releaseRules: [
-            {
-                type: 'revert',
-                scope: '*',
-                release: 'patch'
-            },
-            {
-                type: 'chore',
-                scope: '*',
-                release: 'patch'
-            },
-            {
-                type: 'refactor',
-                scope: '*',
-                release: 'patch'
-            },
-            {
-                type: 'perf',
-                scope: '*',
-                release: 'patch'
-            }
-        ]
-    }
-];
+const commitAnalyzer = '@semantic-release/commit-analyzer';
 
-const releaseNotesGenerator = [
-    '@semantic-release/release-notes-generator',
-    {
-        parserOpts: {
-            noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
-        },
-        presetConfig: {
-            types: [
-                {
-                    type: 'feat',
-                    section: 'Features'
-                },
-                {
-                    type: 'fix',
-                    section: 'Bug Fixes'
-                },
-                {
-                    type: 'revert',
-                    section: 'Internal',
-                    hidden: false
-                },
-                {
-                    type: 'chore',
-                    section: 'Internal',
-                    hidden: false
-                },
-                {
-                    type: 'refactor',
-                    section: 'Internal',
-                    hidden: false
-                },
-                {
-                    type: 'perf',
-                    section: 'Internal',
-                    hidden: false
-                }
-            ]
-        }
-    }
-];
+const releaseNotesGenerator = '@semantic-release/release-notes-generator';
 
 const changelog = '@semantic-release/changelog';
 
@@ -96,7 +31,7 @@ const deprecate = [
 
 const git = '@semantic-release/git';
 
-const teams = ['@argodevops/semantic-release-teams', { packageName: pkg.name }];
+// const teams = ['@argodevops/semantic-release-teams', { packageName: pkg.name }];
 
 module.exports = {
     branches,
@@ -105,8 +40,8 @@ module.exports = {
         releaseNotesGenerator,
         changelog,
         npm,
-//         deprecate,
-        git,
-        teams
+        //         deprecate,
+        git
+        // teams
     ]
 };
