74eac584bbe2f057823d7d53ac0d38b5ba697f18
Ryan GUEST <ryan.b@argodevops.co.uk>
2024-05-15 11:13:56 +0100

    build: fix release step

--- release.config.js
+++ release.config.js
@@ -10,9 +10,8 @@ const commitAnalyzer = '@semantic-release/commit-analyzer';
 const releaseNotesGenerator = '@semantic-release/release-notes-generator';
 const changelog = '@semantic-release/changelog';
 const npm = '@semantic-release/npm';
-const git = '@semantic-release/git';
 
 module.exports = {
     branches,
-    plugins: [commitAnalyzer, releaseNotesGenerator, changelog, npm, git]
+    plugins: [commitAnalyzer, releaseNotesGenerator, changelog, npm]
 };
